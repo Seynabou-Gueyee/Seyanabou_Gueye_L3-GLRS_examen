03a61db94034447d4a606b4e2c0b88ad9fa17eae
Seynabou-Gueyee <seynabou.gueye5@ism.edu.sn>
2025-12-30 15:18:12 +0000

    ZoneType

---
+++ src/Form/ZoneType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Zone;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ZoneType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class, ['label' => 'Nom de la zone'])
+            ->add('prixLivraison', MoneyType::class, ['label' => 'Prix de livraison', 'currency' => 'XOF'])
+            ->add('quartiers', TextareaType::class, ['label' => 'Quartiers (séparés par des virgules)']);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Zone::class,
+        ]);
+    }
+}
