19c1d337447390bd2784c0e5fcb9489e2bbc0541
Seynabou-Gueyee <seynabou.gueye5@ism.edu.sn>
2025-12-30 14:54:14 +0000

    Paiement

---
+++ src/Entity/Paiement.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\Impl\PaiementRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PaiementRepository::class)]
+class Paiement
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $datePaiement = null;
+
+    #[ORM\Column]
+    private ?float $montant = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $methodePaiement = null; // Wave, OM
+
+    #[ORM\OneToOne(inversedBy: 'paiement', cascade: ['persist', 'remove'])]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Commande $commande = null;
+
+    public function __construct()
+    {
+        $this->datePaiement = new \DateTime();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDatePaiement(): ?\DateTimeInterface
+    {
+        return $this->datePaiement;
+    }
+
+    public function setDatePaiement(\DateTimeInterface $datePaiement): static
+    {
+        $this->datePaiement = $datePaiement;
+        return $this;
+    }
+
+    public function getMontant(): ?float
+    {
+        return $this->montant;
+    }
+
+    public function setMontant(float $montant): static
+    {
+        $this->montant = $montant;
+        return $this;
+    }
+
+    public function getMethodePaiement(): ?string
+    {
+        return $this->methodePaiement;
+    }
+
+    public function setMethodePaiement(string $methodePaiement): static
+    {
+        $this->methodePaiement = $methodePaiement;
+        return $this;
+    }
+
+    public function getCommande(): ?Commande
+    {
+        return $this->commande;
+    }
+
+    public function setCommande(Commande $commande): static
+    {
+        $this->commande = $commande;
+        return $this;
+    }
+}
