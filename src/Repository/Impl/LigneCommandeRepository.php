aeb6e5c5d78745f934330a134c18d8b929507453
Seynabou-Gueyee <seynabou.gueye5@ism.edu.sn>
2025-12-30 14:59:08 +0000

    LigneCommandeRepository

---
+++ src/Repository/Impl/LigneCommandeRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Repository\Impl;
+
+use App\Entity\LigneCommande;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+class LigneCommandeRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, LigneCommande::class);
+    }
+}
